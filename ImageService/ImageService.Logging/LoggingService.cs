2575d4fcf90b0e6d2e2be7b1e3bc2ebbef73a994
omerlichter <omerlichter@gmail.com>
2018-03-28 17:05:46 +0300

    add files

---
+++ ImageService/ImageService.Logging/LoggingService.cs
@@ -0,0 +1,19 @@
+﻿
+using ImageService.Logging.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Logging
+{
+    public class LoggingService : ILoggingService
+    {
+        public event EventHandler<MessageRecievedEventArgs> MessageRecieved;
+        public void Log(string message, MessageTypeEnum type)
+        {
+            
+        }
+    }
+}
