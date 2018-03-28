d89298c0d71c45ccd895cc2a07080ba42b05976e
omerlichter <omerlichter@gmail.com>
2018-03-28 16:52:30 +0300

    init

---
+++ ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageService.Modal
+{
+    public class ImageServiceModal : IImageServiceModal
+    {
+        #region Members
+        private string m_OutputFolder;            // The Output Folder
+        private int m_thumbnailSize;              // The Size Of The Thumbnail Size
+       
+        #endregion
+
+    }
+}
